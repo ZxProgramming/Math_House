0ed76b6d2734ee2b93d9937a8b731cf573298816
ahmedYahia73 <ahmedahmadahmid73@gmail.com>
2023-12-12 10:10:26 +0200

    test

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
 Route::get('/', [DashboardController::class, 'index']);
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
