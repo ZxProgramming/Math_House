a7cbfe107d60b03a8b1baab2e425365abf308dc1
ahmedYahia73 <ahmedahmadahmid73@gmail.com>
2023-12-12 10:11:01 +0200

    Hello

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
 Route::get('/', [DashboardController::class, 'index']);
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
