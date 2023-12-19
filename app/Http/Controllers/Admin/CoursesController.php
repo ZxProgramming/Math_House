eb325d535f758d7d10fb0c53fb69c4471100ba4f
ahmedYahia73 <ahmedahmadahmid73@gmail.com>
2023-12-19 13:36:35 +0200

    v4

--- app/Http/Controllers/Admin/CoursesController.php
+++ app/Http/Controllers/Admin/CoursesController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 
 use App\Models\Course;
 use App\Models\Category;
+use App\Models\User;
 
 class CoursesController extends Controller
 {
@@ -33,7 +34,33 @@ class CoursesController extends Controller
 
     public function add_courses(){
         $categories = Category::all();
-        return view('Admin.courses.Courses.AddCourse', compact('categories'));
+        $teachers = User::where('position', 'teacher')
+        ->get();
+        return view('Admin.courses.Courses.AddCourse', 
+        compact('categories', 'teachers'));
+    }
+
+    public function course_add( Request $req ){
+        $arr = $req->only('course_name', 'category_id', 'course_des', 'teacher_id', 
+        'pre_requisition', 'gain', 'duration', 'course_price', 'discount');
+
+        extract($_FILES['course_url']);
+        $img_name = null;
+        if ( !empty($name) ) {
+            $extention_arr = ['jpg', 'jpeg', 'png', 'svg'];
+            $extention = explode('.', $name);
+            $extention = end($extention);
+            $extention = strtolower($extention);
+            if ( in_array($extention, $extention_arr)) {
+                $img_name = now() . rand(1, 10000) . $name;
+                $img_name = str_replace([' ', ':', '-'], 'X', $img_name);
+                $arr['course_url'] = $img_name;
+            }
+        }
+
+        move_uploaded_file($tmp_name, 'images/courses/' . $img_name);
+        Course::create($arr);
+        return redirect()->back();
     }
 
 }
--- resources/views/Admin/courses/Courses/AddCourse.blade.php
+++ resources/views/Admin/courses/Courses/AddCourse.blade.php
@@ -1,50 +1,98 @@
 <x-default-layout>
-@include('Admin.Users.teacher_header')
+@include('Admin.courses.Courses.course_header')
 
-<form class="px-3" action="{{route('add_teacher')}}" method="POST" enctype="multipart/form-data">
+<form class="px-3" action="{{route('course_add')}}" method="POST" enctype="multipart/form-data">
     @csrf
-    <div class='my-3'>
-        <label>Name</label>
-        <input class='form-control' name="name" placeholder="Name" />
-    </div>
-    <div class='my-3'>
-        <label>E-mail</label>
-        <input class='form-control' name="email" placeholder="E-mail" />
-    </div>
-    <div class='my-3'>
-        <label>Phone</label>
-        <input class='form-control' name="phone" placeholder="Phone" />
-    </div>
-    
-    <div class='my-3'>
-        <label>Password</label>
-        <input class='form-control' type="password" name="password" placeholder="Password" />
-    </div> 
-
-    <div class='my-3'>
-        <label>Category</label>
-        <select name="category_id" class="form-control">
-            <option disabled selected>
-                Select Category
-            </option>
-            @foreach($categories as $category)
-            <option value="{{$category->id}}">
-                {{$category->cate_name}}
-            </option>
-            @endforeach
-        </select>
-    </div> 
-
-    <input class="form-control" type="file" name="image" />
-
-    <div class="mt-3">
-        <button class='btn btn-primary'>
-            Submit
+    <div class="info_section">
+        <div class='my-3'>
+            <label>Course Name</label>
+            <input class='form-control' name="course_name" placeholder="Course Name" />
+        </div>
+        <div class='my-3'>
+            <label>Category</label>
+            <select name="category_id" class="form-control">
+                <option disabled selected>
+                    Select Category
+                </option>
+                @foreach($categories as $category)
+                <option value="{{$category->id}}">
+                    {{$category->cate_name}}
+                </option>
+                @endforeach
+            </select>
+        </div> 
+        <div class='my-3'>
+            <label>Description</label>
+            <textarea class='form-control' name="course_des" placeholder="Description" ></textarea>
+        </div>
+        
+        <div class='my-3'>
+            <label>Image</label>
+            <input class='form-control' type="file" name="course_url" placeholder="Image" />
+        </div>
+        <button type="button" class="btn btn-success details_btn">
+            Next
         </button>
-        <button class='btn btn-danger' type="reset">
-            Clear
+    </div>
+
+    <div class="details_section d-none">
+        <div class='my-3'>
+            <label>Teachers</label>
+            <select name="teacher_id" class="form-control">
+                @foreach($teachers as $teacher)
+                <option value="{{$teacher->id}}">
+                    {{$teacher->name}}
+                </option>
+                @endforeach
+            </select>
+        </div>
+        <div class='my-3'>
+            <label>Pre requisition</label>
+            <textarea class='form-control' name="pre_requisition" placeholder="Pre requisition" ></textarea>
+        </div>
+        <div class='my-3'>
+            <label>What you gain</label>
+            <textarea class='form-control' name="gain" placeholder="What you gain" ></textarea>
+        </div>
+        <button type="button" class="btn btn-success pricing_btn">
+            Next
         </button>
+    </div>
+
+    <div class="priceing_section d-none">
+        <div class='my-3'>
+            <label>Duration</label>
+            <input class='form-control' name="duration" placeholder="Duration" />
+        </div>
+        <div class='my-3'>
+            <label>Price</label>
+            <input class='form-control' name="course_price" placeholder="Price" />
+        </div>
+        <div class='my-3'>
+            <label>Discount</label>
+            <input class='form-control' name="discount" placeholder="Discount" />
+        </div>
+        <div class="mt-3">
+            <button class='btn btn-primary'>
+                Submit
+            </button>
+        </div>
     </div>
 </form>
 
+<script>
+    let info_section = document.querySelector('.info_section');
+    let details_section = document.querySelector('.details_section');
+    let priceing_section = document.querySelector('.priceing_section');
+    let details_btn = document.querySelector('.details_btn');
+    let pricing_btn = document.querySelector('.pricing_btn');
+    details_btn.addEventListener('click', () => {
+        info_section.classList.add('d-none');
+        details_section.classList.remove('d-none');
+    });
+    pricing_btn.addEventListener('click', () => {
+        details_section.classList.add('d-none');
+        priceing_section.classList.remove('d-none');
+    });
+</script>
 </x-default-layout>
