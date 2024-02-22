95cd55982f6d6d06b441367cf0dabe03921d9951
Ahmed <ahmedahmadahmid73@gmail.com>
2024-02-22 13:34:47 +0200

    v

--- app/Http/Controllers/Visitor/V_ExamController.php
+++ app/Http/Controllers/Visitor/V_ExamController.php
@@ -11,6 +11,8 @@ use App\Models\Course;
 use App\Models\Category;
 use App\Models\Package;
 use App\Models\User;
+use App\Models\UserPackage;
+use Carbon\Carbon;
 
 class V_ExamController extends Controller
 {
@@ -82,12 +84,26 @@ class V_ExamController extends Controller
         }
         else{
             session()->forget('previous_page');
-            $user_package = User::with('package')
-            ->where('id', auth()->user()->id)
+            $user_package = UserPackage::
+            select('*', 'user_packages.created_at AS package_date')
+            ->leftJoin('users', 'user_packages.user_id', '=', 'users.id')
+            ->leftJoin('packages', 'user_packages.package_id', '=', 'packages.id')
+            ->where('user_packages.user_id', auth()->user()->id)
+            ->where('module', 'Exam')
             ->where('exam_number', '>', 0)
-            ->first();
+            ->get();
+            $duration = false;
+            for ($i=0, $end = count($user_package); $i < $end; $i++) { 
+                $toDate = Carbon::parse(now());
+                $fromDate = Carbon::parse($user_package[$i]->package_date);
+                $days = $toDate->diffInDays($fromDate);
+                
+               if ( $days <= $user_package[$i]->duration ) {
+                $duration = true;
+               }
+            }
             
-            if ( !isset($user_package->package) || count($user_package->package) == 0 ) {
+            if ( empty($user_package) || !$duration ) {
                 $package = Package::
                 where('module', 'Exam')
                 ->get();
@@ -97,7 +113,7 @@ class V_ExamController extends Controller
                 User::with('package')
                 ->where('id', auth()->user()->id)
                 ->update([
-                    'exam_number' => $user_package->exam_number - 1
+                    'exam_number' => $user_package[0]->exam_number - 1
                 ]);
                 return $user_package;
             }
--- app/Http/Controllers/Visitor/V_QuestionController.php
+++ app/Http/Controllers/Visitor/V_QuestionController.php
@@ -8,6 +8,9 @@ use Illuminate\Http\Request;
 use App\Models\Question;
 use App\Models\User;
 use App\Models\Package;
+use App\Models\UserPackage;
+
+use Carbon\Carbon;
 
 class V_QuestionController extends Controller
 {
@@ -67,7 +70,9 @@ class V_QuestionController extends Controller
         return view('Visitor.Question.Filter_Question', 
         compact('q_items'));
     }
-
+    
+        
+ 
     public function q_page( $id ){
         
 
@@ -79,14 +84,28 @@ class V_QuestionController extends Controller
         }
         else{
             session()->forget('previous_page');
-            $user_package = User::with('package')
-            ->where('id', auth()->user()->id)
-            ->where('q_number', '>', 0)
-            ->first();
+            $user_package = UserPackage::
+            select('*', 'user_packages.created_at AS package_date')
+            ->leftJoin('users', 'user_packages.user_id', '=', 'users.id')
+            ->leftJoin('packages', 'user_packages.package_id', '=', 'packages.id')
+            ->where('user_packages.user_id', auth()->user()->id)
+            ->where('module', 'Question')
+            ->where('exam_number', '>', 0)
+            ->get();
+            $duration = false;
+            for ($i=0, $end = count($user_package); $i < $end; $i++) { 
+                $toDate = Carbon::parse(now());
+                $fromDate = Carbon::parse($user_package[$i]->package_date);
+                $days = $toDate->diffInDays($fromDate);
+                
+               if ( $days <= $user_package[$i]->duration ) {
+                $duration = true;
+               }
+            }
             
-            if ( !isset($user_package->package) || count($user_package->package) == 0 ) {
+            if ( empty($user_package) || !$duration ) {
                 $package = Package::
-                where('module', 'Question')
+                where('module', 'Exam')
                 ->get();
                 return view('Student.Exam.Exam_Package', compact('package'));
             }
@@ -94,9 +113,13 @@ class V_QuestionController extends Controller
                 User::with('package')
                 ->where('id', auth()->user()->id)
                 ->update([
-                    'q_number' => $user_package->q_number - 1
+                    'q_number' => $user_package[0]->q_number - 1
                 ]);
-                return $user_package;
+
+                $question = Question::where('id', $id)
+                ->first();
+
+                return view('Visitor.Question.Show_Question', compact('question'));
             }
         }
     }
