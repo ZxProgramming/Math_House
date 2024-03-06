c37c1f63472bfaed58e22fb52c61803c5b2875a9
Ahmed <ahmedahmadahmid73@gmail.com>
2024-03-06 13:49:19 +0200

    v

--- resources/views/Student/Exam_History/Exam_History.blade.php
+++ resources/views/Student/Exam_History/Exam_History.blade.php
@@ -42,12 +42,35 @@
                     <div class="row g-0  p-3">
                         @foreach ( $item->exams->question as $item )
                             @if ( !empty($item->question) )
-                                {{$item->question}}
+                            {{$item->question}}
                             @endif
                             @if ( !empty($item->q_url) )
                             <img style="width: 200px; height: 200px;"
                                 src="{{ asset('images/questions/' . $item->q_url) }}" />
                             @endif
+                            <button class="btn btn-primary ans_item_btn my-2">View Answer</button>
+                            <div class="ans_item d-none">
+                                <b> Answer :
+                                @if ( $item->ans_type == 'MCQ' )
+                                    {{$item->mcq[0]->mcq_answers}}
+                                @else 
+                                {{$item->g_ans[0]->grid_ans}}
+                                @endif
+                                </b>
+                                <br />
+                                @foreach ( $item->q_ans as $q_ans )
+                                @if ( !empty($q_ans->ans_video) )
+                                <iframe width="560" height="315" src="{{$q_ans->ans_video}}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                            
+                                @endif
+                                @endforeach
+                              </div>
+                            @foreach ( $item->q_ans as $q_ans)
+                            <a href="{{asset('files/q_pdf/' . $q_ans->ans_pdf)}}" class="btn btn-success my-2" download>Download Pdf {{$loop->iteration}}</a>
+                            @endforeach
+            
+                            <a href="{{route('question_parallel', ['id' => $item->id])}}" class="btn btn-info my-2" >Solve Parallel</a>
+            
                             <hr />
                         @endforeach
                     </div>
