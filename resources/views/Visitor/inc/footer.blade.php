79bc1363072988df71cd57564981b5326463fb51
Ahmed <ahmedahmadahmid73@gmail.com>
2024-03-18 11:27:44 +0200

    v

--- resources/views/Visitor/inc/footer.blade.php
+++ resources/views/Visitor/inc/footer.blade.php
@@ -76,8 +76,8 @@
 			<div class="row">
 				<div class="col-sm-4 col-md-3 col-lg-3 col-xl-2 pb15 pt15">
 					<div class="logo-widget home1">
-						<img class="img-fluid" src="images/header-logo.png" alt="header-logo.png">
-						<span>EDUMY</span>
+						<img class="img-fluid" style="width: 130px;height: 70px;" src="{{asset('assets/media/logos/Maths-house.png')}}" alt="header-logo.png">
+						
 					</div>
 				</div>
 				<div class="col-sm-8 col-md-5 col-lg-6 col-xl-6 pb25 pt25 brdr_left_right">
@@ -113,7 +113,7 @@
 			<div class="row">
 				<div class="col-lg-6 offset-lg-3">
 					<div class="copyright-widget text-center">
-						<p>Copyright Edumy © 2019. All Rights Reserved.</p>
+						<p>Copyright MathsHouse © {{date('Y')}}. All Rights Reserved.</p>
 					</div>
 				</div>
 			</div>
